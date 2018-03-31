b554589e9ff8f156d263a6360f8fb401fc9cc446
Daniel Ramos <danielrz@protonmail.com>
2018-03-30 21:32:02 -0400

    Fix timezone

--- project/settings/common.py
+++ project/settings/common.py
@@ -175,7 +175,7 @@ LOGIN_REDIRECT_URL = '/'
 # =============================================================================
 
 LANGUAGE_CODE = 'es'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Asuncion'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
